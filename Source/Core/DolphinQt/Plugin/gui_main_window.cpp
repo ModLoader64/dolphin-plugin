f35dad0f887e7e8d692f6592c1eebb1c35c681a0
maraon64 <17965220+maraon64@users.noreply.github.com>
2023-08-17 23:20:58 +0200

    set main window icon

--- Source/Core/DolphinQt/Plugin/gui_main_window.cpp
+++ Source/Core/DolphinQt/Plugin/gui_main_window.cpp
@@ -25,6 +25,11 @@ EXPORT void gui_main_window_show()
   g_main_window->Show();
 }
 
+EXPORT void gui_main_window_set_icon_from_file(const char* path)
+{
+  g_main_window->setWindowIcon(QIcon{QString::fromUtf8(path)});
+}
+
 EXPORT void gui_main_window_start_game_from_file(const char* path)
 {
   g_main_window->StartGame(BootParameters::GenerateFromFile(path));
